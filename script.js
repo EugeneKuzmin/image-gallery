566fae5864e8ecb052169d1d3d0bd2ae6145cae3
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-09-28 14:43:58 +0300

    fix:key word search bug fix

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+const apiKey = 'k3Z6TXPNkSfDiCZT9u1wD6tyMJzvWbXcUDJWFbr4XJo'
+
+const unsplash_root = 'https://api.unsplash.com'
+
 const images = document.querySelectorAll('[role="image"]')
 const inptSearch = document.getElementById('inptSearch')
 const btnSearch = document.getElementById('btnSearch')
 
 async function getData() {
-    const res = await fetch(`https://api.unsplash.com/photos?per_page=3&query=${String(inptSearch.value).trim}&orientation=portrait&client_id=k3Z6TXPNkSfDiCZT9u1wD6tyMJzvWbXcUDJWFbr4XJo`);
+    const res = await fetch(`${unsplash_root}/search/photos?page=1&per_page=3&query=${inptSearch.value}&orientation=portrait&client_id=${apiKey}`);
+    // const res = await fetch('https://api.unsplash.com/photos?per_page=3&query=water&orientation=portrait&client_id=k3Z6TXPNkSfDiCZT9u1wD6tyMJzvWbXcUDJWFbr4XJo');
+
     const data = await res.json();
+
     for (let pic = 0; pic < 3; pic++) {
-        images[pic].src = data[pic].urls.small
+        images[pic].src = data.results[pic].urls.small
     }
   }
   btnSearch.addEventListener('click',()=>{getData()})
