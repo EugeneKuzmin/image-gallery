bd85b95feb6cc1b951fbfdb4b4266f67e90f100e
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-09-29 19:02:12 +0300

    feat:Add search button. Add close button. Create page dynamicaly from js

--- script.js
+++ script.js
@@ -2,18 +2,30 @@ const apiKey = 'k3Z6TXPNkSfDiCZT9u1wD6tyMJzvWbXcUDJWFbr4XJo'
 
 const unsplash_root = 'https://api.unsplash.com'
 
-const images = document.querySelectorAll('[role="image"]')
+const gridContainer = document.querySelector('.grid-container')
 const inptSearch = document.getElementById('inptSearch')
 const btnSearch = document.getElementById('btnSearch')
+const btnClose = document.getElementById('btnClose')
 
 async function getData() {
     const res = await fetch(`${unsplash_root}/search/photos?page=1&per_page=3&query=${inptSearch.value}&orientation=portrait&client_id=${apiKey}`);
-    // const res = await fetch('https://api.unsplash.com/photos?per_page=3&query=water&orientation=portrait&client_id=k3Z6TXPNkSfDiCZT9u1wD6tyMJzvWbXcUDJWFbr4XJo');
 
     const data = await res.json();
 
     for (let pic = 0; pic < 3; pic++) {
-        images[pic].src = data.results[pic].urls.small
+        const card = document.createElement('div')
+        const img = document.createElement('img')
+        card.classList.add('picture-card');
+        card.classList.add('stacked');
+        img.src = data.results[pic].urls.small
+        card.appendChild(img)
+        gridContainer.appendChild(card)
     }
   }
   btnSearch.addEventListener('click',()=>{getData()})
+
+  function inputClear(){
+    inptSearch.value = ''
+  }
+
+  btnClose.addEventListener('click',()=>{inputClear()})
