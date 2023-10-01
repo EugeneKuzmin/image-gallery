86f99590fc9488cd5dcaa23166c0a3288d1863a3
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-10-01 19:19:53 +0300

    fix:image propper size fix. Add image counter

--- script.js
+++ script.js
@@ -2,30 +2,79 @@ const apiKey = 'k3Z6TXPNkSfDiCZT9u1wD6tyMJzvWbXcUDJWFbr4XJo'
 
 const unsplash_root = 'https://api.unsplash.com'
 
+const container = document.querySelector('.container')
 const gridContainer = document.querySelector('.grid-container')
 const inptSearch = document.getElementById('inptSearch')
 const btnSearch = document.getElementById('btnSearch')
 const btnClose = document.getElementById('btnClose')
+const arrowLeft = document.getElementById('arrow-left')
+const arrowRight = document.getElementById('arrow-right')
+const pictureNumber = document.getElementById('pictureNumber')
 
-async function getData() {
-    const res = await fetch(`${unsplash_root}/search/photos?page=1&per_page=3&query=${inptSearch.value}&orientation=portrait&client_id=${apiKey}`);
+let btnCloseHidden = true
 
-    const data = await res.json();
 
-    for (let pic = 0; pic < 3; pic++) {
-        const card = document.createElement('div')
-        const img = document.createElement('img')
-        card.classList.add('picture-card');
-        card.classList.add('stacked');
-        img.src = data.results[pic].urls.small
-        card.appendChild(img)
-        gridContainer.appendChild(card)
-    }
+async function getData(searchTxt) {
+  
+  const res = await fetch(`${unsplash_root}/search/photos?page=1&per_page=${pictureNumber.value}&query=${searchTxt}&client_id=${apiKey}`);
+
+  const data = await res.json();
+  gridContainer.innerHTML = ''
+
+  for (let pic = 0; pic <= pictureNumber.value; pic++) {
+      const card = document.createElement('div')
+      const img = document.createElement('img')
+      card.classList.add('picture-card');
+      img.classList.add('picture-class')
+      // Math.random()<0.5?card.classList.add('portrait'):card.classList.add('landscape')
+      img.src = data.results[pic].urls.small
+      card.appendChild(img)
+      gridContainer.appendChild(card)
   }
-  btnSearch.addEventListener('click',()=>{getData()})
+}
 
-  function inputClear(){
-    inptSearch.value = ''
+btnSearch.addEventListener('click',()=>{
+
+  getData(inptSearch.value)
+})
+
+inptSearch.addEventListener('input',()=>{
+  if(inptSearch.value.length&&btnCloseHidden){
+    btnCloseHidden = false
+    btnClose.classList.remove('hide')
+  }else if(!inptSearch.value.length){
+    btnClose.classList.add('hide')
+    btnCloseHidden = true
+  }
+})
+
+inptSearch.addEventListener('keydown',(e)=>{
+  if(e.code === 'Enter'){
+    getData(inptSearch.value)
   }
 
-  btnClose.addEventListener('click',()=>{inputClear()})
+})
+
+function inputClear(){
+  inptSearch.value = ''
+  btnCloseHidden = true
+  btnClose.classList.add('hide')
+}
+
+btnClose.addEventListener('click',()=>{inputClear()})
+
+function initLoad(){
+  getData('random')
+}
+
+arrowLeft.addEventListener('click',
+()=>{
+  if(pictureNumber.value>0) pictureNumber.value--
+})
+
+arrowRight.addEventListener('click',
+()=>{
+  if(pictureNumber.value<=20) pictureNumber.value++
+})
+
+initLoad()
